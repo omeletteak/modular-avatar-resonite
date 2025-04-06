9061bce4bd1b81cbe5cf33aa1f22ab8379ccf101
bd_ <bd_@nadena.dev>
2025-04-06 14:28:56 -0700

    ci: testing new build workflow

--- DevTools~/SyncToUnity.ps1
+++ DevTools~/SyncToUnity.ps1
@@ -3,4 +3,7 @@ if ($LASTEXITCODE -ne 0) {
     exit $LASTEXITCODE
 }
 
+New-Item -Path ".\ResoPuppet~" -ItemType Directory -Force
 Copy-Item -Path "Resonite~\ResoniteHook\ResoPuppetSchema\bin\Debug\*" -Destination ".\Managed" -Recurse -Force
+Copy-Item -Path "Resonite~\ResoniteHook\Launcher\bin\Debug\*" -Destination ".\ResoPuppet~" -Recurse -Force
+Copy-Item -Path "Resonite~\ResoniteHook\Puppeteer\bin\Debug\*" -Destination ".\ResoPuppet~" -Recurse -Force
