7d69d1e5e44ff27aee17046e8d8a67a36986bfd9
bd_ <bd_@nadena.dev>
2025-04-06 14:40:10 -0700

    ci: testing new build workflow

--- DevTools~/SyncToUnity.ps1
+++ DevTools~/SyncToUnity.ps1
@@ -5,5 +5,8 @@ if ($LASTEXITCODE -ne 0) {
 
 New-Item -Path ".\ResoPuppet~" -ItemType Directory -Force
 Copy-Item -Path "Resonite~\ResoniteHook\ResoPuppetSchema\bin\Debug\*" -Destination ".\Managed" -Recurse -Force
-Copy-Item -Path "Resonite~\ResoniteHook\Launcher\bin\Debug\*" -Destination ".\ResoPuppet~" -Recurse -Force
-Copy-Item -Path "Resonite~\ResoniteHook\Puppeteer\bin\Debug\*" -Destination ".\ResoPuppet~" -Recurse -Force
+Copy-Item -Path "Resonite~\ResoniteHook\Launcher\bin\Debug\net9.0\*.dll" -Destination ".\ResoPuppet~" -Recurse -Force
+Copy-Item -Path "Resonite~\ResoniteHook\Launcher\bin\Debug\net9.0\*.exe" -Destination ".\ResoPuppet~" -Recurse -Force
+Copy-Item -Path "Resonite~\ResoniteHook\Launcher\bin\Debug\net9.0\*.pdb" -Destination ".\ResoPuppet~" -Recurse -Force
+Copy-Item -Path "Resonite~\ResoniteHook\Puppeteer\bin\Debug\net9.0\*.dll" -Destination ".\ResoPuppet~" -Recurse -Force
+Copy-Item -Path "Resonite~\ResoniteHook\Puppeteer\bin\Debug\net9.0\*.pdb" -Destination ".\ResoPuppet~" -Recurse -Force
