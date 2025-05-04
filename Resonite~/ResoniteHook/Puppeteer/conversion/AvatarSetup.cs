1aacc3ae4fdfed4dba1ccfda8c821b5cfdea5412
bd_ <bd_@nadena.dev>
2025-05-04 16:51:53 -0700

    feat: add an AvatarRenderSettings settings node (#49)
    Closes: #32

--- Resonite~/ResoniteHook/Puppeteer/conversion/AvatarSetup.cs
+++ Resonite~/ResoniteHook/Puppeteer/conversion/AvatarSetup.cs
@@ -115,6 +115,7 @@ public partial class RootConverter
         Defer(PHASE_POSTPROCESS, () => new EyeSwingVariableFilter(_context).Apply());
         Defer(PHASE_POSTPROCESS, () => new FaceMeshReferenceFilter(_context).Apply(spec));
         Defer(PHASE_POSTPROCESS, () => new ThumbnailAssetProviderFilter(_context).Apply());
+        Defer(PHASE_POSTPROCESS, () => new RenderSettingsFilter(_context).Apply());
         Defer(PHASE_POSTPROCESS, () => new HandIsolationFilter(_context).Apply());
         Defer(PHASE_RESOLVE_REFERENCES, () => new BoneAnnotationsFilter(_context).Apply(spec));
 
---
+++ Resonite~/ResoniteHook/Puppeteer/filters/RenderSettingsFilter.cs
@@ -0,0 +1,13 @@
+﻿using FrooxEngine.CommonAvatar;
+using nadena.dev.resonity.remote.puppeteer.rpc;
+
+namespace nadena.dev.resonity.remote.puppeteer.filters;
+
+public class RenderSettingsFilter(TranslateContext context)
+{
+    public void Apply()
+    {
+        var settingsNode = context.SettingsNode.AddSlot("RenderSettings");
+        settingsNode.AttachComponent<AvatarRenderSettings>();
+    }
+}
