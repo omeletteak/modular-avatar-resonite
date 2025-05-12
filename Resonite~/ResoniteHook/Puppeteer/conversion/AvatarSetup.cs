851a5aafd315f9710067ee35e65dd7ddff9544ab
bd_ <bd_@nadena.dev>
2025-05-11 20:18:46 -0700

    feat: improve handling of A-pose avatars (#27)
    Handling isn't perfect yet; in particular, the desktop hand poses are a bit off. This needs testing in VR as well.
    
    Closes: #15

--- Resonite~/ResoniteHook/Puppeteer/conversion/AvatarSetup.cs
+++ Resonite~/ResoniteHook/Puppeteer/conversion/AvatarSetup.cs
@@ -270,6 +270,68 @@ public partial class RootConverter
         }
     }
 
+    struct HandCoordinates
+    {
+        public float3 up;
+        public float3 forward;
+    }
+
+    private HandCoordinates GetHandCoordinates(p.Arm arm)
+    {
+        // Sometimes, models can end up with weird hand positions as a result of VRIK processing. As such we need to
+        // figure out if the hand is facing up or down. To do this, we take the cross product of the wrist-to-thumb and
+        // wrist-to-index vectors. This gives us a vector that is perpendicular to the plane of the hand.
+
+        var wrist = Object<f.Slot>(arm.Hand);
+        var thumb = Object<f.Slot>(arm.Thumb.LastBone());
+        var index = Object<f.Slot>(arm.Index.LastBone());
+        
+        if (wrist == null || thumb == null || index == null) return new HandCoordinates()
+        {
+            // We have no hand coordinates, so make a guess
+            up = float3.Up,
+            forward = float3.Forward
+        };
+        
+        var cross = Vector3.Cross(
+            index.GlobalPosition - wrist.GlobalPosition,
+            thumb.GlobalPosition - wrist.GlobalPosition
+        );
+
+        return new HandCoordinates()
+        {
+            up = AxisAlignDirection(wrist, cross),
+            forward = AxisAlignDirection(wrist, index.GlobalPosition - wrist.GlobalPosition)
+        };
+
+        float3 AxisAlignDirection(f.Slot slot, float3 direction)
+        {
+            direction = direction.Normalized;
+
+            float3 bestGlobalDirection = float3.Zero;
+            AlignAxis(ref bestGlobalDirection, float3.Up);
+            AlignAxis(ref bestGlobalDirection, float3.Right);
+            AlignAxis(ref bestGlobalDirection, float3.Forward);
+
+            return bestGlobalDirection;
+            
+            void AlignAxis(ref float3 bestGlobalDirection, float3 axis)
+            {
+                var bestDot = Vector3.Dot(bestGlobalDirection, direction);
+                
+                var globalDir = slot.LocalDirectionToGlobal(axis);
+                var currentDot = Vector3.Dot(globalDir, direction);
+
+                if (currentDot < 0)
+                {
+                    globalDir = -globalDir;
+                    currentDot = -currentDot;
+                }
+
+                if (currentDot > bestDot) bestGlobalDirection = globalDir;
+            }
+        }
+    }
 
     private async Task InvokeAvatarBuilder(f.Slot slot, p.AvatarDescriptor spec)
     {
@@ -300,7 +362,8 @@ public partial class RootConverter
         var bone_left_hand = Object<f.Slot>(spec.Bones.LeftArm.Hand);
         var bone_right_hand = Object<f.Slot>(spec.Bones.RightArm.Hand);
 
-        var handLength = MeasureHandSize(spec.Bones.LeftArm);
+        var handLength = MeasureHandSize(spec.Bones.LeftArm, out var leftFinger);
+        MeasureHandSize(spec.Bones.RightArm, out var rightFinger);
         if (handLength < 0.01f) handLength = baseScale;
         var handScale = (2f/3f) * handLength / baseScale;
         
@@ -320,21 +383,28 @@ public partial class RootConverter
         Console.WriteLine("Headset fwd vector: " + slot_headset.GlobalDirectionToLocal(Vector3.UnitZ));
         
         // Align hands. The resonite (right) hand model has the Z axis facing along the fingers, and Y up.
-        // First, find the corresponding axis for the model's hand.
+        
+        var leftHandCoords = GetHandCoordinates(spec.Bones.LeftArm);
+        var rightHandCoords = GetHandCoordinates(spec.Bones.RightArm);
+        
         var rightArm = bone_right_hand.Parent;
         var leftArm = bone_left_hand.Parent;
-        var rightHandFwd = rightArm.LocalDirectionToGlobal(bone_right_hand.LocalPosition.Normalized);
-        var leftHandFwd = leftArm.LocalDirectionToGlobal(bone_left_hand.LocalPosition.Normalized);
-        var rightPointPosition = bone_right_hand.GlobalPosition + (rightHandFwd - float3.Up) * (handLength / 2);
-        var leftPointPosition = bone_left_hand.GlobalPosition + (leftHandFwd - float3.Up) * (handLength / 2);
+        float3 rightHandFwd = rightHandCoords.forward;
+        float3 leftHandFwd = leftHandCoords.forward;
+
+        float3 rightHandUp = rightHandCoords.up;
+        float3 leftHandUp = leftHandCoords.up;
+        
+        var rightPointPosition = bone_right_hand.GlobalPosition + (rightHandFwd - rightHandUp) * (handLength / 2);
+        var leftPointPosition = bone_left_hand.GlobalPosition + (leftHandFwd - leftHandUp) * (handLength / 2);
         
         
         // Now use a look rotation to align the model's hand with the resonite hand
         var rightHandRot = Quaternion.CreateFromRotationMatrix(
-            Matrix4x4.CreateLookAt(Vector3.Zero, rightHandFwd, Vector3.UnitY)
+            Matrix4x4.CreateLookAt(Vector3.Zero, rightHandFwd, rightHandUp)
         );
         var leftHandRot = Quaternion.CreateFromRotationMatrix(
-            Matrix4x4.CreateLookAt(Vector3.Zero, leftHandFwd, Vector3.UnitY)
+            Matrix4x4.CreateLookAt(Vector3.Zero, leftHandFwd, leftHandUp)
         );
 
         Console.WriteLine("Update wait...");
@@ -381,16 +451,13 @@ public partial class RootConverter
         var m_alignAnchors = avatarCreator.GetType()
             .GetMethod("AlignAnchors", BindingFlags.NonPublic | BindingFlags.Instance);
 
-        m_alignAnchors!.Invoke(avatarCreator, [slot_left_hand]);
-        m_alignAnchors.Invoke(avatarCreator, [slot_right_hand]);
-
         slot_left_point.Position_Field.ActiveLink?.ReleaseLink();
         slot_right_point.Position_Field.ActiveLink?.ReleaseLink();
         slot_left_point.GlobalPosition = leftPointPosition;
         slot_right_point.GlobalPosition = rightPointPosition;
 
-        SetAnchorPositions(slot_left_hand, leftHandFwd);
-        SetAnchorPositions(slot_right_hand, rightHandFwd);
+        SetAnchorPositions(slot_left_hand, leftHandFwd, leftHandUp);
+        SetAnchorPositions(slot_right_hand, rightHandFwd, rightHandUp);
         
         // TODO - scale adjustment
         
@@ -404,38 +471,70 @@ public partial class RootConverter
             headProxy.LocalPosition = spec.EyePosition.Vec3();
             target.GlobalPosition = origTargetPos;
         }
+
+        SetToolshelfPosition(bone_left_hand, leftHandFwd, leftHandUp);
+        SetToolshelfPosition(bone_right_hand, rightHandFwd, rightHandUp);
         
         
-        void SetAnchorPositions(f.Slot hand, float3 fwd)
+        void SetAnchorPositions(f.Slot hand, float3 fwd, float3 up)
         {
             var tooltip = hand.FindChild("Tooltip");
             var grabber = hand.FindChild("Grabber");
             //var shelf = hand.FindChild("Shelf"); // TODO
 
             tooltip.GlobalPosition = hand.GlobalPosition + fwd * handLength * 1.2f;
-            grabber.GlobalPosition = hand.GlobalPosition + fwd * (handLength / 2f) - float3.Up * (handLength / 2);
+            tooltip.GlobalRotation = floatQ.LookRotation(fwd, up);
+            grabber.GlobalPosition = hand.GlobalPosition + fwd * (handLength / 2f) - up * (handLength / 2);
+            grabber.GlobalRotation = tooltip.GlobalRotation;
         }
     }
 
-    private float MeasureHandSize(p.Arm bonesLeftArm)
+    private void SetToolshelfPosition(f.Slot slotHand, float3 forward, float3 up)
     {
+        // position: 0.05m above wrist (for now) 
+        // Z+: up from the wrist
+        // Y+: Towards elbow
+        
+        var toolAnchor = slotHand.GetComponentsInChildren<f.CommonAvatar.AvatarToolAnchor>()
+            .First(a => a.AnchorPoint.Value == AvatarToolAnchor.Point.Toolshelf)
+            .Slot;
+        toolAnchor.GlobalRotation = floatQ.LookRotation(forward, up);
+        
+        toolAnchor.GlobalPosition = slotHand.GlobalPosition + up * 0.05f;
+        
+        // Undo the effects of the toolshelf's local position
+        var toolshelfLocalPos = new float3(0.02f, 0.01f, -0.14f);
+        toolAnchor.GlobalPosition = toolAnchor.LocalPointToGlobal(-toolshelfLocalPos);
+    }
+
+    private float MeasureHandSize(p.Arm bonesLeftArm, out f.Slot? longestFinger)
+    {
+        longestFinger = null;
+        
         var hand = Object<f.Slot>(bonesLeftArm.Hand);
         if (hand == null) throw new Exception("Hand not found");
-        
-        float length = MeasureFinger(bonesLeftArm.Index);
-        length = Math.Max(length, MeasureFinger(bonesLeftArm.Middle));
-        length = Math.Max(length, MeasureFinger(bonesLeftArm.Ring));
-        return Math.Max(length, MeasureFinger(bonesLeftArm.Pinky));
 
-        float MeasureFinger(p.Finger finger)
+        float length = 0;
+        MeasureFinger(bonesLeftArm.Index, ref length, ref longestFinger);
+        MeasureFinger(bonesLeftArm.Middle, ref length, ref longestFinger);
+        MeasureFinger(bonesLeftArm.Ring,ref length,  ref longestFinger);
+        MeasureFinger(bonesLeftArm.Pinky, ref length, ref longestFinger);
+        return length;
+
+        void MeasureFinger(p.Finger finger, ref float length, ref f.Slot? longestFinger)
         {
             var boneRef = finger.LastBone();
-            if (boneRef == null) return 0;
+            if (boneRef == null) return;
             var lastBone = Object<f.Slot>(boneRef);
 
-            if (lastBone == null) return 0;
+            if (lastBone == null) return;
 
-            return (hand.GlobalPosition - lastBone.GlobalPosition).Magnitude;
+            var len = (hand.GlobalPosition - lastBone.GlobalPosition).Magnitude;
+            if (len > length)
+            {
+                length = len;
+                longestFinger = lastBone;
+            }
         }
     }
 
