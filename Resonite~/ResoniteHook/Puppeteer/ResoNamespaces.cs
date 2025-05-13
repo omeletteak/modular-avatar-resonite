1313a51e275ad4cefaeed379e3ce5925b07133b2
bd_ <bd_@nadena.dev>
2025-05-12 21:26:47 -0700

    chore: adjust humanoid bone reference naming (#74)

--- Resonite~/ResoniteHook/Puppeteer/ResoNamespaces.cs
+++ Resonite~/ResoniteHook/Puppeteer/ResoNamespaces.cs
@@ -8,8 +8,8 @@ public class ResoNamespaces
     public const string AvatarRoot = ModularAvatarNamespacePrefix + "AvatarRoot";
     public const string SettingsRoot = ModularAvatarNamespacePrefix + "AvatarSettingsRoot";
     
-    public const string HumanBoneRef = ModularAvatarNamespacePrefix + "BoneRef_";
-    public const string HumanBonePose = ModularAvatarNamespacePrefix + "BonePose_";
+    public const string HumanBoneRef = ModularAvatarNamespacePrefix + "HumanBone.";
+    public const string HumanBonePose = ModularAvatarNamespacePrefix + "HumanBonePose.";
     public const string Config_EyeSwing = ModularAvatarNamespacePrefix + "EyeSwing";
     public const string Config_ThumbnailAssetProvider = ModularAvatarNamespacePrefix + "ThumbnailAssetProvider";
     public const string Config_ThumbnailAssetProviderCrop = ModularAvatarNamespacePrefix + "ThumbnailAssetProvider_Crop";
