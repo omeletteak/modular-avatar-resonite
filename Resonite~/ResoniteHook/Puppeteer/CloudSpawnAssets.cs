1fc3cc8502ee89f901e1f0d3ad944eb0e662ad61
bd_ <bd_@nadena.dev>
2025-05-16 20:15:16 -0700

    fix: loading thumbnail is huge (#86)

--- Resonite~/ResoniteHook/Puppeteer/CloudSpawnAssets.cs
+++ Resonite~/ResoniteHook/Puppeteer/CloudSpawnAssets.cs
@@ -3,5 +3,5 @@
 public static class CloudSpawnAssets
 {
     public const string CoreSystems = "resrec:///U-1Nj73SRfaDY/R-bd749fb1-2e9d-481c-b6d2-4bf76da071dd";
-    public const string LoadingDisplay = "resrec:///U-1Nj73SRfaDY/R-9f9335d1-bd74-487e-aed5-aa8212f18bd9";
+    public const string LoadingDisplay = "resrec:///U-1Nj73SRfaDY/R-b894b868-2fe3-4494-9cdd-03990fcc4461";
 }
