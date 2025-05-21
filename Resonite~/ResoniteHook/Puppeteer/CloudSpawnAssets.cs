87e22c7668403dc74fd1c6d90b9aae3fa06a1a54
bd_ <bd_@nadena.dev>
2025-05-20 22:24:25 -0700

    fix: incorrect CoreSystems reference (#97)

--- Resonite~/ResoniteHook/Puppeteer/CloudSpawnAssets.cs
+++ Resonite~/ResoniteHook/Puppeteer/CloudSpawnAssets.cs
@@ -2,6 +2,6 @@
 
 public static class CloudSpawnAssets
 {
-    public const string CoreSystems = "resrec:///U-1Nj73SRfaDY/R-43e3d80e-6dfe-4775-a5e1-03cbba253493";
+    public const string CoreSystems = "resrec:///U-1Nj73SRfaDY/R-bd27525f-ad17-4ec7-9b1e-eea5696fc0be";
     public const string LoadingDisplay = "resrec:///U-1Nj73SRfaDY/R-b894b868-2fe3-4494-9cdd-03990fcc4461";
 }
