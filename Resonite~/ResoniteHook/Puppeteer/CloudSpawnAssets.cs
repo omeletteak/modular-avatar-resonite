00f7cfde0c306bbd8b68a4b979eb5e6e8929873c
bd_ <bd_@nadena.dev>
2025-05-27 21:29:16 -0700

    chore: update cloud spawn assets (#112)
    - Fix: AvatarWorn detection breaks when culling systems are active
    - Fix: Loading display size depends on the orientation of your head

--- Resonite~/ResoniteHook/Puppeteer/CloudSpawnAssets.cs
+++ Resonite~/ResoniteHook/Puppeteer/CloudSpawnAssets.cs
@@ -2,6 +2,6 @@
 
 public static class CloudSpawnAssets
 {
-    public const string CoreSystems = "resrec:///U-1Nj73SRfaDY/R-bd27525f-ad17-4ec7-9b1e-eea5696fc0be";
-    public const string LoadingDisplay = "resrec:///U-1Nj73SRfaDY/R-b894b868-2fe3-4494-9cdd-03990fcc4461";
+    public const string CoreSystems = "resrec:///U-1Nj73SRfaDY/R-2d67b0f7-d459-4017-8860-0fc49433ed27";
+    public const string LoadingDisplay = "resrec:///U-1Nj73SRfaDY/R-139d104e-8a40-49d6-99df-eadee6dd42db";
 }
