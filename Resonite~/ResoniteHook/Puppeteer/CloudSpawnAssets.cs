334867532ce13877a35292cb69fec5cf5c5e2ef8
bd_ <bd_@nadena.dev>
2025-06-19 19:28:50 -0800

    fix: loading standin spawns at the wrong position (#136)

--- Resonite~/ResoniteHook/Puppeteer/CloudSpawnAssets.cs
+++ Resonite~/ResoniteHook/Puppeteer/CloudSpawnAssets.cs
@@ -3,5 +3,5 @@
 public static class CloudSpawnAssets
 {
     public const string CoreSystems = "resrec:///U-1Nj73SRfaDY/R-2d67b0f7-d459-4017-8860-0fc49433ed27";
-    public const string LoadingDisplay = "resrec:///U-1Nj73SRfaDY/R-139d104e-8a40-49d6-99df-eadee6dd42db";
+    public const string LoadingDisplay = "resrec:///U-1Nj73SRfaDY/R-67a7437e-fab7-4cb2-9e49-a5f9ff17bbe8";
 }
